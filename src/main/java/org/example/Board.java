56d93b77409dc5919c245b9c949bd127cdad97fe
pezzi <pezzi.hartlieb@gmail.com>
2024-05-16 16:54:41 +0200

    fixed board constructor

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,7 +3,12 @@ package org.example;
 public class Board {
     public char[][] cells;
 
-    public Board(char[][] cells) {
-        this.cells = cells;
+    public Board() {
+        cells = new char[3][3];
+        for (int i = 0; i<3;i++){
+            for (int j = 0; j<3;j++) {
+                cells[i][j] = ' ';
+            }
+        }
     }
 }
