39d932eddf273865e89ef60d2a3ac0c74682eb46
OnSebii <lang.sebastian.006@gmail.com>
2024-05-23 13:50:07 +0200

    CellEmpty & place  function
    Added function to check if a cell is empty and a function to place a symbol

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -11,4 +11,14 @@ public class Board {
             }
         }
     }
+
+    public char isCellEmpty(int x, int y) {
+        return cells[x][y] = ' ';
+    }
+
+    public void place(int x, int y, char marker) {
+        if (isCellEmpty(x, y) == ' ') {
+            cells[x][y] = marker;
+        }
+    }
 }
