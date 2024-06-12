742e448511f59ee37c7d924c4b90af22d1d04e38
pezzi <pezzi.hartlieb@gmail.com>
2024-06-12 11:58:01 +0200

    added first test

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,8 +1,5 @@
 import org.example.Board;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TicTacToeTest {
